1ad0a19a03158a286837de0692007dd657d1b729
jamestrimbleafa <114614928+jamestrimbleafa@users.noreply.github.com>
2023-02-23 13:26:33 -0700

    Added script to convert TF v1 models to TF Lite.  Started working on Docker image and script to run the TFLite model on a RPi

---
+++ src/TF1-TFLiteConversion.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+tf.compat.v1.disable_v2_behavior()
+
+converter = tf.compat.v1.lite.TFLiteConverter.from_saved_model("C:\\Users\\James.Trimble\\OneDrive - afacademy.af.edu\\Documents\\499\\AI Accelerator\\Models\\droneInfGraph401092\\saved_model\\")
+converter.target_spec.supported_ops = [
+  tf.lite.OpsSet.TFLITE_BUILTINS, # enable TensorFlow Lite ops.
+  tf.lite.OpsSet.SELECT_TF_OPS # enable TensorFlow ops.
+]
+tflite_model = converter.convert()
+open("converted_model.tflite","wb").write(tflite_model)
