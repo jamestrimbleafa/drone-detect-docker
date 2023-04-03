7d218b29533e994bf7fe0dfb6a9deced3df7cdcc
James Trimble <114614928+jamestrimbleafa@users.noreply.github.com>
2023-04-03 16:03:06 -0600

    Added TF Record generation

--- retrain/src/RetrainModel.sh
+++ retrain/src/RetrainModel.sh
@@ -4,7 +4,7 @@ TEST_PATH=/app/DroneTestDataset
 
 # Check if that the datasets exist in /app/DroneTrainDataset and /app/DroneTestDataset (i.e. folders are not empty)
 if [ -d "$TRAIN_PATH" ]; then
-    if [ "$(ls -A $TRAIN_PATH)"]; then
+    if [ "$(ls -A $TRAIN_PATH)" ]; then
         echo "Training Data found."
     else
         echo "Training Data not found."
@@ -33,7 +33,7 @@ if [ -f "$TRAIN_PATH/drone_train_labels.csv" ]; then
 else
     # If no CSV is found, rebuild the CSV file.
     echo "Training CSV not found.  Building CSV."
-    python xml_to_csv.py $TRAIN_PATH/Drone_TrainSet_XMLs/ $TRAIN_PATH/drone_train_labels.csv
+    python3 xml_to_csv.py $TRAIN_PATH/Drone_TrainSet_XMLs/ $TRAIN_PATH/drone_train_labels.csv
 fi
 
 if [ -f "$TEST_PATH/drone_test_labels.csv" ]; then
@@ -41,10 +41,26 @@ if [ -f "$TEST_PATH/drone_test_labels.csv" ]; then
 else
     # If no CSV is found, rebuild the CSV file.
     echo "Testing CSV not found.  Building CSV."
-    python xml_to_csv.py $TEST_PATH/Drone_TestSet_XMLs/ $TEST_PATH/drone_test_labels.csv
+    python3 xml_to_csv.py $TEST_PATH/Drone_TestSet_XMLs/ $TEST_PATH/drone_test_labels.csv
 fi
 
 
 # Build the TFRecords from the csv files and images
+# Check that the csv files exist.  If not, regenerate them.
+if [ -f "$TRAIN_PATH/train.record" ]; then
+    echo "Training TFRecord found."
+else
+    # If no CSV is found, rebuild the CSV file.
+    echo "Training TFRecord not found.  Building TFRecord."
+    python3 generate_tfrecord.py --csv_input=$TRAIN_PATH/drone_train_labels.csv  --image_dir=$TRAIN_PATH/Drone_TrainSet/ --output_path=$TRAIN_PATH/train.record
+fi
+
+if [ -f "$TEST_PATH/test.record" ]; then
+    echo "Testing TFRecord found."
+else
+    # If no CSV is found, rebuild the CSV file.
+    echo "Testing TFRecord not found.  Building TFRecord."
+    python3 generate_tfrecord.py --csv_input=$TEST_PATH/drone_test_labels.csv  --image_dir=$TEST_PATH/Drone_TestSet/ --output_path=$TEST_PATH/test.record
+fi
 
 # 
